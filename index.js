abce6ecde6ea184373b7753f5d251e0fef0f4302
Ty Nguyen <tynguyen.tech@gmail.com>
2016-09-03 07:11:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function iterativeLog(array){
+  array.forEach((element,index)=>
+         {console.log(`${index}: ${element}`)})
+}
+
+function iterate(callback){
+  var array = [1,2,4,5]
+  array.forEach(callback)
+  return array
+}
+
+function doToArray(array, callback){
+  array.forEach(callback)
+}
